0cd02748a10e3663b383f7ff9e215a743fdec148
ASII3R <asierpozo737@gmail.com>
2026-04-27 12:19:51 +0200

    feat: documentación javascript

--- php/js/hero.js
+++ php/js/hero.js
@@ -1,16 +1,24 @@
 document.addEventListener("DOMContentLoaded", function () {
 
+    // Botón de "Entrar"
     const enterBtn = document.getElementById("enterBtn");
+
+    // Contenedor de los roles (admin,profe y tecnic)
     const roles = document.getElementById("roles");
 
+    // Para cuando haces click en entrar: 
     enterBtn.addEventListener("click", function (e) {
+
+        // Evita comportamiento por defecto (por ejemplo, link)
         e.preventDefault();
 
-        // Oculta botón entrar
+        // eliminar boton entrar
         enterBtn.remove();
 
-        // Muestra roles
+        // Muestra el bloque de roles
         roles.classList.remove("d-none");
+
+        // Añadir estilos
         roles.classList.add("hero-roles", "mt-3");
     });
 
--- php/js/professor.js
+++ php/js/professor.js
@@ -1,11 +1,19 @@
 document.addEventListener("DOMContentLoaded", function () {
+
+    // Selecciona la caja del profesor del HTML
     const box = document.querySelector(".professor-box");
 
+    // Si existe la caja:
     if (box) {
+
+        // Estado inicial: invisible y hacia abajo
         box.style.opacity = "0";
         box.style.transform = "translateY(20px)";
+
+        // Transición
         box.style.transition = "all 0.5s ease";
 
+        // Después de 100ms hace la animación
         setTimeout(() => {
             box.style.opacity = "1";
             box.style.transform = "translateY(0)";
