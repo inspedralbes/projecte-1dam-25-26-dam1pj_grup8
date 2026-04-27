ad476e78920de43fd3027ac1944d02f5ca1f4de2
ASII3R <asierpozo737@gmail.com>
2026-04-27 11:41:56 +0200

    feat: responsive + css

--- php/footer.php
+++ php/footer.php
@@ -8,6 +8,7 @@
     </div>
 </footer>
 
+<!-- JS -->
 <script src="js/hero.js"></script>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
--- php/header.php
+++ php/header.php
@@ -7,6 +7,9 @@
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/admin.css">
+    <link rel="stylesheet" href="css/professor.css">
+    <link rel="stylesheet" href="css/tecnic.css">
 </head>
 
 <body>
